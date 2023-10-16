9da504b70041f5634b1ca8e2e676196490df99e5
Ackap <ackapga@gmail.com>
2023-10-16 16:41:16 +0600

    Add style

--- jQuery/index.php
+++ jQuery/index.php
@@ -5,48 +5,68 @@
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>jQuery</title>
-
+        <style><?php echo file_get_contents("style.css"); ?></style>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script>
-
-            function funcBefore() {
-                $("#info").text("Ожидание данных...");
-            }
-
-            function funcSuccess(data) {
-                $("#info").text(data)
-            }
-
-            $(document).ready (function () {
-                $("#load").bind("click", function () {
-                    $.ajax ({
-                        url: "check.php",
-                        type: "POST",
-                        data: ({name: $("#name").val()}),
-                        dataType: "html",
-                        beforeSend: function () {
-                            $("#info").text("Ожидание данных...");
-                        },
-                        success: function (data) {
-                            if(data == "Fail")
-                                $("#info").text(data);
-                            else
-                                $("#info").text(data);
-                        }
-                    });
-                });
-            });
-        </script>
-
     </head>
 
     <body>
+    <div class="content">
+        <div class="box_jQuery">
 
-    <input type="text" id="name" placeholder="Введите имя">
+            <span id="status" class="status">status</span>
 
-    <input type="button" id="load" value="Отправить">
+            <input type="text" id="name" class="input" placeholder="Введите имя">
 
-    <div id="info"></div>
+            <input type="button" id="load" class="load" value="Отправить">
 
+            <div id="info" class="info">text</div>
+
+        </div>
+    </div>
     </body>
+
+    <script>
+
+        function funcBefore() {
+            $("#info").text("Ожидание данных...");
+        }
+
+        function funcSuccess(data) {
+            $("#info").text(data)
+        }
+
+        $(document).ready (function () {
+            $("#load").bind("click", function () {
+                let admin = "ADMIN";
+                $.ajax ({
+                    url: "content.php",
+                    type: "POST",
+                    data: ({name: admin, number: 5}),
+                    dataType: "html",
+                    beforeSend: funcBefore,
+                    success: funcSuccess
+                })
+            });
+        });
+
+        $(document).ready (function () {
+            $("#load").bind("click", function () {
+                $.ajax ({
+                    url: "check.php",
+                    type: "POST",
+                    data: ({name: $("#name").val()}),
+                    dataType: "html",
+                    beforeSend: function () {
+                        $("#info").text("Ожидание данных...");
+                    },
+                    success: function (data) {
+                        if(data == "Fail")
+                            $("#info").text(data);
+                        else
+                            $("#info").text(data);
+                    }
+                });
+            });
+        });
+    </script>
 </html>

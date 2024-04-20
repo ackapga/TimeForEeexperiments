df00588ad384de53527302f0b84cba243d272fc6
ackapga2 <ackapga@gmail.com>
2024-04-20 10:54:04 +0500

    Some Fix

--- script_26_soft_change_content_all_file.php
+++ script_26_soft_change_content_all_file.php
@@ -1,28 +1,94 @@
 <?php
 $directories = [
+    __DIR__,
+    'js',
+    'js/modules',
+    'js/products',
+
     'aktau',
+    'aktau/js',
+    'aktau/js/products',
+
     'aktobe',
+    'aktobe/js',
+    'aktobe/js/products',
+
     'astana',
+    'astana/js',
+    'astana/js/products',
+
     'atyrau',
+    'atyrau/js',
+    'atyrau/js/products',
+
     'janaozen',
+    'janaozen/js',
+    'janaozen/js/products',
+
     'jezkazgan',
+    'jezkazgan/js',
+    'jezkazgan/js/products',
+
     'karaganda',
+    'karaganda/js',
+    'karaganda/js/products',
+
     'kokshetau',
+    'kokshetau/js',
+    'kokshetau/js/products',
+
     'kostanai',
+    'kostanai/js',
+    'kostanai/js/products',
+
     'kyzylorda',
+    'kyzylorda/js',
+    'kyzylorda/js/products',
+
     'pavlodar',
+    'pavlodar/js',
+    'pavlodar/js/products',
+
     'petropavlovsk',
+    'petropavlovsk/js',
+    'petropavlovsk/js/products',
+
     'semei',
+    'semei/js',
+    'semei/js/products',
+
     'shymkent',
+    'shymkent/js',
+    'shymkent/js/products',
+
     'taldykorgan',
+    'taldykorgan/js',
+    'taldykorgan/js/products',
+
     'taraz',
+    'taraz/js',
+    'taraz/js/products',
+
     'turkestan',
+    'turkestan/js',
+    'turkestan/js/products',
+
     'uralsk',
+    'uralsk/js',
+    'uralsk/js/products',
+
     'ust-kamenogorsk',
+    'ust-kamenogorsk/js',
+    'ust-kamenogorsk/js/products',
 ];
 
 foreach ($directories as $dir) {
-    echo PHP_EOL . ' START DIRECTORY -------------------> ' . $dir . '. <br> ' . PHP_EOL;
+    echo PHP_EOL . '--------------------------------------------------> START DIRECTORY | ' . $dir . ' <br> ' . PHP_EOL;
+
+    if (!is_readable($dir)) {
+        echo PHP_EOL . '!!! ----------> ACCESS NOT DIR | ' . $dir . ' <br> ' . PHP_EOL;
+        continue;
+    }
 
     $filesToChange = scandir($dir);
 
@@ -30,13 +96,14 @@ foreach ($directories as $dir) {
 
     foreach ($filesToChange as $file) {
 
-        if ($file === '.' || $file === '..' || $file === $selfFile) {
+        $wayToFile = $dir . DIRECTORY_SEPARATOR . $file;
+
+        if (!is_readable($wayToFile) || $file === '.' || $file === '..' || $file === $selfFile) {
+            echo PHP_EOL . '!!!!!!!!!! ----------> ACCESS NOT FILE | ' . $wayToFile . ' <br> ' . PHP_EOL;
             continue;
         }
 
-        $wayToFile = $dir . DIRECTORY_SEPARATOR . $file;
-
-        echo PHP_EOL . ' FILE -----> ' . $wayToFile . ' <br> ' . PHP_EOL;
+        echo PHP_EOL . '---------->  FILE | ' . $wayToFile . ' <br> ';
 
         changeContentItemSS($wayToFile);
 
@@ -50,10 +117,9 @@ function changeContentItemSS($fileWhichChange)
     $content = file_get_contents($fileWhichChange);
     $content = changeContentItemCanSome($content);
     if (file_put_contents($fileWhichChange, $content)) {
-        echo PHP_EOL . ' Change CHFPHAIW&TP ---> ' . $fileWhichChange . '. <br> ' . PHP_EOL;
+        echo PHP_EOL . '---> success | ' . $fileWhichChange . '. <br> ' . PHP_EOL;
     } else {
-        echo PHP_EOL . ' STOP CHANGE CHFPHAIW&TP ---> ' . $fileWhichChange . '. <br> ' . PHP_EOL;
-        die();
+        echo PHP_EOL . '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! WTF ---> ' . $fileWhichChange . ' <br> ' . PHP_EOL;
     }
 }
 
@@ -64,6 +130,5 @@ function changeContentItemCanSome($fileContent)
         '',
         $fileContent);
 }
-
 
 /*--------------------------------------------------------------------------------------------------------------------*/
